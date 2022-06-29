8decb0887b147515125a75b59742459f5162a648
Max Hawkins <37495064+max-Hawkins@users.noreply.github.com>
2022-06-29 11:41:18 -0700

    Eliminate vectorized acceleration and intermediate normalization

--- src/cpu.jl
+++ src/cpu.jl
@@ -1,5 +1,3 @@
-using LoopVectorization
-
 circmod(x,y) = mod(x-1,y) + 1
 
 function dedisp(source::AbstractMatrix{T}, plan) where {T <: Real}
@@ -7,11 +5,11 @@ function dedisp(source::AbstractMatrix{T}, plan) where {T <: Real}
     _, n_dm = size(plan)
     output = zeros(T, n_samp, n_dm)
     μ = mean(source)
-    @tturbo for i in 1:n_dm
+    for i in 1:n_dm
         for k in 1:n_samp
             for j in 1:n_chan
                 shifted_samp_idx = circmod(k + plan[j,i],n_samp)
-                output[k, i] += source[shifted_samp_idx, j] / n_samp
+                output[k, i] += source[shifted_samp_idx, j]
             end
         end
     end
